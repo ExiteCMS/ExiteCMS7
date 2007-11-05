47582f34165af8c780895a64f136e725da3db325
hverton <hverton@f05d0674-473a-0410-9a19-83e10c34e0e8>
2007-11-05 20:03:15 +0000

    The user_info_panel module has a dependency with the forum_threads_list_panel module. This dependency is now checked, and links will be replaced my plain text if the module is not installed.

--- modules/user_info_panel/templates/modules.user_info_panel.tpl
+++ modules/user_info_panel/templates/modules.user_info_panel.tpl
@@ -38,9 +38,13 @@
 	{if $new_post_msg != 0}
 		<hr />
 		<div style='text-align:center'>
-			<a href='{$smarty.const.MODULES}forum_threads_list_panel/new_posts.php' class='side'><b>{$new_post_msg|string_format:$locale.090}{if $new_post_msg == 1}{$locale.088}{else}{$locale.089}{/if}</b></a>
-			<hr />
-			{buttonlink name=$locale.091 link=$smarty.const.MODULES|cat:"forum_threads_list_panel/new_posts.php?markasread="|cat:$user_id}
+			{if $new_posts_panel}
+				<a href='{$smarty.const.MODULES}forum_threads_list_panel/new_posts.php' class='side'><b>{$new_post_msg|string_format:$locale.090}{if $new_post_msg == 1}{$locale.088}{else}{$locale.089}{/if}</b></a>
+				<hr />
+				{buttonlink name=$locale.091 link=$smarty.const.MODULES|cat:"forum_threads_list_panel/new_posts.php?markasread="|cat:$user_id}
+			{else}
+				<b>{$new_post_msg|string_format:$locale.090}{if $new_post_msg == 1}{$locale.088}{else}{$locale.089}{/if}</b>
+			{/if}
 		</div>
 	{/if}
 {else}
--- modules/user_info_panel/user_info_panel.php
+++ modules/user_info_panel/user_info_panel.php
@@ -18,10 +18,10 @@ if (eregi("user_info_panel.php", $_SERVER['PHP_SELF']) || !defined('INIT_CMS_OK'
 $variables = array();
 
 if (isset($userdata['user_id'])) {
-	$variables['new_pm_msg'] = dbcount("(pmindex_id)", "pm_index", "pmindex_user_id ='".$userdata['user_id']."' AND pmindex_to_id='".$userdata['user_id']."' AND pmindex_read_datestamp = '0'");
-	$variables['new_post_msg'] = dbcount("(post_id)", "posts_unread", "user_id='".$userdata['user_id']."'");
+	// store the user's ID and name
 	$variables['user_id'] = isset($userdata['user_id'])?$userdata['user_id']:0;
 	$variables['user_name'] = isset($userdata['user_name'])?$userdata['user_name']:"";
+	// if an administrator, check to which pages this admin has access to
 	if (iADMIN) {
 		$usr_rghts = " (admin_rights='".str_replace(".", "' OR admin_rights='", $userdata['user_rights'])."')";
 		$variables['adminpage1'] = dbcount("(*)", "admin", $usr_rghts." AND admin_link!='reserved' AND admin_page='1'");
@@ -29,6 +29,11 @@ if (isset($userdata['user_id'])) {
 		$variables['adminpage3'] = dbcount("(*)", "admin", $usr_rghts." AND admin_link!='reserved' AND admin_page='3'");
 		$variables['adminpage4'] = dbcount("(*)", "admin", $usr_rghts." AND admin_link!='reserved' AND admin_page='4'");
 	}
+	$variables['new_pm_msg'] = dbcount("(pmindex_id)", "pm_index", "pmindex_user_id ='".$userdata['user_id']."' AND pmindex_to_id='".$userdata['user_id']."' AND pmindex_read_datestamp = '0'");
+	$variables['new_post_msg'] = dbcount("(post_id)", "posts_unread", "user_id='".$userdata['user_id']."'");
+	// check if the forum_threads_list_panel module is installed
+	$result = dbquery("SELECT * FROM ".$db_prefix."modules WHERE mod_folder = 'forum_threads_list_panel'");
+	$variables['new_posts_panel'] = dbrows($result);
 }
 $variables['loginerror'] = isset($loginerror) ? $loginerror : "";
 $variables['remember_me'] = isset($_COOKIE['remember_me'])?$_COOKIE['remember_me']:"no";
