20ae4c4fb638fb7281005d57d0a95741ac112e6b
hverton <hverton@f05d0674-473a-0410-9a19-83e10c34e0e8>
2008-02-09 21:26:35 +0000

    modified all modules for use with ExiteCMS v7.1 removed all locale files

--- modules/welcome_message_panel/module_installer.php
+++ modules/welcome_message_panel/module_installer.php
@@ -1,12 +1,10 @@
 <?php
 /*---------------------------------------------------+
-| PLi-Fusion Content Management System               |
+| ExiteCMS Content Management System                 |
 +----------------------------------------------------+
-| Copyright 2007 WanWizard (wanwizard@gmail.com)     |
-| http://www.pli-images.org/pli-fusion               |
+| Copyright 2007 Harro "WanWizard" Verton, Exite BV  |
+| for support, please visit http://exitecms.exite.eu |
 +----------------------------------------------------+
-| Some portions copyright ? 2002 - 2006 Nick Jones   |
-| http://www.php-fusion.co.uk/                       |
 | Released under the terms & conditions of v2 of the |
 | GNU General Public License. For details refer to   |
 | the included gpl.txt file or visit http://gnu.org  |
@@ -14,17 +12,11 @@
 if (!checkrights("I") || !defined("iAUTH") || $aid != iAUTH || !defined('INIT_CMS_OK')) fallback(BASEDIR."index.php");
 
 /*---------------------------------------------------+
-| Locale definition for this installation module     |
-+----------------------------------------------------*/
-
-// no locales for this plugin
-
-/*---------------------------------------------------+
 | Module identification                              |
 +----------------------------------------------------*/
 $mod_title = "Welcome message panel";
 $mod_description = "Body panel to display the site's welcome message";
-$mod_version = "1.0.0";
+$mod_version = "1.1.0";
 $mod_developer = "WanWizard";
 $mod_email = "wanwizard@gmail.com";
 $mod_weburl = "http://exitecms.exite.eu/";
@@ -35,6 +27,7 @@ $mod_type = "P";
 +----------------------------------------------------*/
 
 $mod_folder = "welcome_message_panel";
+
 // no administration module for this plugin
 
 /*---------------------------------------------------+
@@ -46,8 +39,8 @@ if (str_replace(".", "", $settings['version']) < 700) {
 	$mod_errors .= sprintf($locale['mod001'], '7.00');
 }
 // check for a maximum version of the ExiteCMS engine
-if (str_replace(".", "", $settings['version']) > 700) {
-	$mod_errors .= sprintf($locale['mod002'], '7.00');
+if (str_replace(".", "", $settings['version']) > 710) {
+	$mod_errors .= sprintf($locale['mod002'], '7.10');
 }
 // check for a specific revision number range that is supported
 if ($settings['revision'] < 0 || $settings['revision'] > 999999) {
@@ -59,6 +52,12 @@ if ($settings['revision'] < 0 || $settings['revision'] > 999999) {
 +----------------------------------------------------*/
 
 $mod_site_links = array();
+
+/*---------------------------------------------------+
+| locale strings for this module                     |
++----------------------------------------------------*/
+
+$localestrings = array();
 
 /*---------------------------------------------------+
 | commands to execute when installing this module    |
@@ -77,6 +76,19 @@ $mod_uninstall_cmds = array();
 +----------------------------------------------------*/
 if (!function_exists('module_upgrade')) {
 	function module_upgrade($current_version) {
+
+		global $db_prefix, $locale;
+
+		switch($current_version) {
+			case "0.0.1":
+				// pre-release version, no database or other changes
+			case "1.0.0":
+				// ExiteCMS v7.0. no upgrade actions for this release
+			case "1.1.0":
+				// upgrade to ExiteCMS v7.1. no upgrade actions for this release
+			default:
+				// do this at every upgrade
+		}
 	}
 }
 ?>
