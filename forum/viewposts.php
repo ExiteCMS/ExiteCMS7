a6b61fe9318b081c8c582bd1027ff62e468488f0
hverton <hverton@f05d0674-473a-0410-9a19-83e10c34e0e8>
2008-05-15 15:56:35 +0000

    Added an uption to view all posts (newest first) - added missing viewposts.php

---
+++ forum/viewposts.php
@@ -0,0 +1,72 @@
+<?php
+/*---------------------------------------------------+
+| ExiteCMS Content Management System                 |
++----------------------------------------------------+
+| Copyright 2007 Harro "WanWizard" Verton, Exite BV  |
+| for support, please visit http://exitecms.exite.eu |
++----------------------------------------------------+
+| Some portions copyright 2002 - 2006 Nick Jones     |
+| http://www.php-fusion.co.uk/                       |
++----------------------------------------------------+
+| Released under the terms & conditions of v2 of the |
+| GNU General Public License. For details refer to   |
+| the included gpl.txt file or visit http://gnu.org  |
++----------------------------------------------------*/
+require_once dirname(__FILE__)."/../includes/core_functions.php";
+require_once PATH_ROOT."/includes/theme_functions.php";
+
+// temp storage for template variables
+$variables = array();
+
+// defines
+define('ITEMS_PER_PAGE', 20);
+
+// load the forum functions include
+require_once PATH_INCLUDES."forum_functions_include.php";
+
+// set the panel title
+$title = $locale['027a'];
+
+// check if we have a forum ID to filter on
+if (!isset($forum_id) || !isNum($forum_id)) $forum_id = false;
+
+// check if we have anything to display
+$result = dbquery(
+	"SELECT tp.*, tf.* FROM ".$db_prefix."posts tp
+	INNER JOIN ".$db_prefix."forums tf USING(forum_id)
+	WHERE ".groupaccess('forum_access').($forum_id ? " AND tp.forum_id = '$forum_id'" : "")
+);
+$rows = dbrows($result);
+$variables['rows'] = $rows;
+
+// make sure rowstart has a valid value
+if (!isset($rowstart) || !isNum($rowstart)) $rowstart = 0;
+$variables['rowstart'] = $rowstart;
+
+$result = dbquery(
+	"SELECT tp.*, tf.* FROM ".$db_prefix."posts tp
+	INNER JOIN ".$db_prefix."forums tf USING(forum_id)
+	WHERE ".groupaccess('forum_access').($forum_id ? " AND tp.forum_id = '$forum_id'" : "")."
+	ORDER BY post_datestamp DESC 
+	LIMIT $rowstart,".ITEMS_PER_PAGE
+);
+
+$posts = array();
+while ($data = dbarray($result)) {
+	$data['poll'] = fpm_panels_poll_exists($data['forum_id'], $data['thread_id']);
+	$posts[] = $data;
+}
+$variables['posts'] = $posts;
+
+// pagenav url
+if ($forum_id) {
+	$variables['pagenav_url'] = FUSION_SELF."?forum_id=".$forum_id."&amp;";
+}
+
+// define the search body panel variables
+$template_panels[] = array('type' => 'body', 'name' => 'viewposts', 'title' => $title, 'template' => 'forum.viewposts.tpl', 'locale' => 'forum.main');
+$template_variables['viewposts'] = $variables;
+
+// Call the theme code to generate the output for this webpage
+require_once PATH_THEME."/theme.php";
+?>
