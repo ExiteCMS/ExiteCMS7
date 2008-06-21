84424050facdcf2b5f7a02dd84c8b79ea26937b2
hverton <hverton@f05d0674-473a-0410-9a19-83e10c34e0e8>
2008-06-21 21:13:33 +0000

    add a country code to the posts record, to avoid GeoIP lookups when a post is displayed

---
+++ administration/upgrade/rev01477.php
@@ -0,0 +1,55 @@
+<?php
+/*---------------------------------------------------+
+| ExiteCMS Content Management System                 |
++----------------------------------------------------+
+| Copyright 2007 Harro "WanWizard" Verton, Exite BV  |
+| for support, please visit http://exitecms.exite.eu |
++----------------------------------------------------+
+| Released under the terms & conditions of v2 of the |
+| GNU General Public License. For details refer to   |
+| the included gpl.txt file or visit http://gnu.org  |
++----------------------------------------------------*/
+
+// upgrade for revision
+$_revision = '1477';
+
+if (eregi("rev".substr("00000".$_revision,-5).".php", $_SERVER['PHP_SELF']) || !defined('INIT_CMS_OK')) die();
+
+// make sure the required array's exist
+if (!isset($revisions) || !is_array($revisions)) $revisions = array();
+if (!isset($commands) || !is_array($commands)) $commands = array();
+
+// register this revision update
+$revisions[] = array('revision' => $_revision, 
+					'date' => mktime(19,00,0,6,21,2008), 
+					'title' => "Required updates for ExiteCMS v7.1 rev.".$_revision,
+					'description' => "Added the country code to post records to avoid unnecessary GeoIP lookups.");
+
+// array to store the commands of this update
+$commands = array();
+
+// add an index on the locales table to speed up some queries
+$commands[] = array('type' => 'db', 'value' => "ALTER TABLE ##PREFIX##posts ADD post_cc CHAR(2) NOT NULL AFTER post_ip");
+
+// and make sure the ranking image directory exists
+$commands[] = array('type' => 'function', 'value' => "add_cc_to_posts");
+
+/*---------------------------------------------------+
+| functions required for part of the upgrade process |
++----------------------------------------------------*/
+function add_cc_to_posts() {
+	global $db_prefix;
+
+	require_once PATH_INCLUDES."geoip_include.php";
+
+	// get all posts records
+	$result = dbquery("SELECT post_id, post_ip FROM ".$db_prefix."posts");
+	while ($data = dbarray($result)) {
+		$cc = GeoIP_IP2Code($data['post_ip']);
+		if ($cc) {
+			$result2 = dbquery("UPDATE ".$db_prefix."posts SET post_cc = '".$cc."' WHERE post_id = '".$data['post_id']."'");
+		}
+	}
+}
+
+?>
