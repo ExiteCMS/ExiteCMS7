35d2d7b04775ca7302dbb80a1e88ff776cfc049c
hverton <hverton@f05d0674-473a-0410-9a19-83e10c34e0e8>
2008-08-06 15:40:48 +0000

    remove the link to the registration page if it is already present in the menu

--- modules/user_info_panel/user_info_panel.php
+++ modules/user_info_panel/user_info_panel.php
@@ -18,9 +18,11 @@ if (eregi("user_info_panel.php", $_SERVER['PHP_SELF']) || !defined('INIT_CMS_OK'
 $variables = array();
 
 if (iMEMBER) {
+
 	// store the user's ID and name
 	$variables['user_id'] = isset($userdata['user_id'])?$userdata['user_id']:0;
 	$variables['user_name'] = isset($userdata['user_name'])?$userdata['user_name']:"";
+
 	// if an administrator, check to which pages this admin has access to
 	if (iADMIN) {
 		$result = dbquery("SELECT admin_page, count(*) AS count FROM ".$db_prefix."admin WHERE"." (admin_rights='".str_replace(".", "' OR admin_rights='", $userdata['user_rights'])."')"." AND admin_link != 'reserved' GROUP BY admin_page");
@@ -29,8 +31,10 @@ if (iMEMBER) {
 		}
 		$variables['adminpage5'] = checkrights("T");
 	}
+
 	// new PM messages
 	$variables['new_pm_msg'] = dbcount("(pmindex_id)", "pm_index", "pmindex_user_id ='".$userdata['user_id']."' AND pmindex_to_id='".$userdata['user_id']."' AND pmindex_read_datestamp = '0'");
+
 	// new forum messages
 	if ($userdata['user_posts_unread']) {
 		$result = dbquery("
@@ -60,10 +64,12 @@ if (iMEMBER) {
 			);
 	} 
 	$variables['new_post_msg'] = ($result ? mysql_result($result, 0) : 0);
+
 	// check if the forum_threads_list_panel module is installed
 	$result = dbquery("SELECT * FROM ".$db_prefix."modules WHERE mod_folder = 'forum_threads_list_panel'");
 	$variables['new_posts_panel'] = dbrows($result);
 }
+
 $variables['loginerror'] = isset($loginerror) ? $loginerror : "";
 $variables['remember_me'] = isset($_SESSION['remember_me']) ? $_SESSION['remember_me'] : "no";
 $variables['login_expiry']  = (iADMIN && isset($_SESSION['login_expire'])) ? time_system2local($_SESSION['login_expire']) : "";
@@ -80,8 +86,23 @@ foreach($variables['auth_methods'] as $key => $method) {
 	}
 }
 
+// check if we need to display a registration link
+if ($settings['enable_registration']) {
+	$variables['show_reglink'] = true;
+	// get all menu items for this user
+	$linkinfo = array();
+	menu_generate_tree("", array(1,2,3), false);
+	foreach ($linkinfo as $link) {
+		if ($link['link_url'] == "/register.php") {
+			$variables['show_reglink'] = false;
+			break;
+		}
+	}
+} else {
+	$variables['show_reglink'] = false;
+}
+
 // check if we need to display links
-$variables['show_reglink'] = $settings['enable_registration'];
 $variables['show_passlink'] = 1;
 
 $template_variables['modules.user_info_panel'] = $variables;
