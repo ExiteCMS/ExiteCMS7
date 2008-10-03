f9d31f83f37c753c7c113caf193e2a7282d86034
hverton <hverton@f05d0674-473a-0410-9a19-83e10c34e0e8>
2008-10-03 14:55:21 +0000

    updated PHPmailer to v2.0.2

--- includes/language/phpmailer.lang-ro.php
+++ includes/language/phpmailer.lang-ro.php
@@ -1,24 +1,25 @@
 <?php
 /**
- * PHPMailer language file.  
+ * PHPMailer language file.
  * Romanian Version
  * @package PHPMailer
  * @author Catalin Constantin <catalin@dazoot.ro>
- * @actualizare Forum IT http://www.forumit.ro/
  */
 
 $PHPMAILER_LANG = array();
 
-$PHPMAILER_LANG["provide_address"] = 'Trebuie s&#259; ad&#259;uga&#355;i cel pu&#355;in un recipient (adres&#259; de mail).';
+$PHPMAILER_LANG["provide_address"]      = 'Trebuie sa adaugati cel putin un recipient (adresa de mail).';
 $PHPMAILER_LANG["mailer_not_supported"] = ' mailer nu este suportat.';
-$PHPMAILER_LANG["execute"] = 'Nu pot executa:  ';
-$PHPMAILER_LANG["instantiate"] = 'Nu am putut instan&#355;ia func&#355;ia mail.';
-$PHPMAILER_LANG["authenticate"] = 'Eroare SMTP: Nu a func&#355;ionat autentificarea.';
-$PHPMAILER_LANG["from_failed"] = 'Urm&#259;toarele adrese From au dat eroare: ';
-$PHPMAILER_LANG["recipients_failed"] = 'Eroare SMTP: Urm&#259;toarele adrese de mail au dat eroare: ';
-$PHPMAILER_LANG["data_not_accepted"] = 'Eroare SMTP: Con&#355;inutul mailului nu a fost acceptat.';
-$PHPMAILER_LANG["connect_host"] = 'Eroare SMTP: Nu m-am putut conecta la adresa SMTP.';
-$PHPMAILER_LANG["file_access"] = 'Nu pot accesa fi&#351;ierul: ';
-$PHPMAILER_LANG["file_open"] = 'Eroare de fi&#351;ier: Nu pot deschide fi&#351;ierul: ';
-$PHPMAILER_LANG["encoding"] = 'Codare necunoscut&#259;: ';
+$PHPMAILER_LANG["execute"]              = 'Nu pot executa:  ';
+$PHPMAILER_LANG["instantiate"]          = 'Nu am putut instantia functia mail.';
+$PHPMAILER_LANG["authenticate"]         = 'Eroare SMTP: Nu a functionat autentificarea.';
+$PHPMAILER_LANG["from_failed"]          = 'Urmatoarele adrese From au dat eroare: ';
+$PHPMAILER_LANG["recipients_failed"]    = 'Eroare SMTP: Urmatoarele adrese de mail au dat eroare: ';
+$PHPMAILER_LANG["data_not_accepted"]    = 'Eroare SMTP: Continutul mailului nu a fost acceptat.';
+$PHPMAILER_LANG["connect_host"]         = 'Eroare SMTP: Nu m-am putut conecta la adresa SMTP.';
+$PHPMAILER_LANG["file_access"]          = 'Nu pot accesa fisierul: ';
+$PHPMAILER_LANG["file_open"]            = 'Eroare de fisier: Nu pot deschide fisierul: ';
+$PHPMAILER_LANG["encoding"]             = 'Encodare necunoscuta: ';
+$PHPMAILER_LANG["signing"]              = 'Signing Error: ';
+
 ?>
