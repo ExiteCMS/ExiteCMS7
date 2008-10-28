7d28b21d59c90dd32233633862c416c33db695e2
WanWizard <WanWizard@f05d0674-473a-0410-9a19-83e10c34e0e8>
2008-10-28 09:08:31 +0000

    fixed typo causing a PHP error

--- forum/post.php
+++ forum/post.php
@@ -597,7 +597,7 @@ if (isset($_POST["cancel"])) {
 									$error = $locale['431'];
 									$result = dbquery("INSERT INTO ".$db_prefix."flood_control (flood_ip, flood_userid, flood_timestamp) VALUES ('".USER_IP."', '".$userdata['user_id']."', '".time()."')");
 									if (dbcount("(flood_ip)", "flood_control", "flood_ip='".USER_IP."' AND flood_userid='".$userdata['user_id']."'") > 4) {
-`										$result = dbquery("UPDATE ".$db_prefix."users SET user_status='1', user_ban_reason='".$locale['434']."' WHERE user_id='".$userdata['user_id']."'");
+										$result = dbquery("UPDATE ".$db_prefix."users SET user_status='1', user_ban_reason='".$locale['434']."' WHERE user_id='".$userdata['user_id']."'");
 										$error .= "<br />".$locale['432'];
 									} else {
 										$error .= "<br />".sprintf($locale['433'], $settings['flood_interval']);
