079fc6f1146dc72a208913be8b7579d6947613ac
WanWizard <WanWizard@f05d0674-473a-0410-9a19-83e10c34e0e8>
2008-11-29 00:46:57 +0000

    modified bot detection code

--- includes/user_functions.php
+++ includes/user_functions.php
@@ -30,9 +30,13 @@ if (FUSION_SELF != "setuser.php" && dbcount("(*)", "blacklist", "blacklist_ip='"
 }
 
 // check for bot users
-$_bot_list = array("Teoma", "alexa", "froogle", "Gigabot", "inktomi","looksmart", "URL_Spider_SQL", "Firefly", "NationalDirectory","Ask Jeeves", "TECNOSEEK", "InfoSeek", "WebFindBot", "girafabot","crawler", "www.galaxy.com", "Googlebot", "Scooter", "Slurp","msnbot", "appie", "FAST", "WebBug", "Spade", "ZyBorg", "rabaz","Baiduspider", "Feedfetcher-Google", "TechnoratiSnoop", "Rankivabot","Mediapartners-Google", "Sogou web spider", "WebAlta Crawler", "Twiceler", "curl/");
+$_bot_list = array("Teoma","alexa","froogle","Gigabot","inktomi","looksmart","URL_Spider_SQL","Firefly","NationalDirectory","Ask Jeeves","TECNOSEEK",
+					"InfoSeek","WebFindBot","girafabot","crawler","www.galaxy.com","Googlebot","Scooter","Slurp","msnbot","appie","FAST","WebBug",
+					"Spade","ZyBorg","rabaz","Baiduspider","Feedfetcher-Google","TechnoratiSnoop","Rankivabot","Mediapartners-Google","Sogou web spider",
+					"WebAlta Crawler","Twiceler","curl/"
+				);
 foreach($_bot_list as $bot) {
-	if(ereg($bot, $_SERVER['HTTP_USER_AGENT'])) {
+	if(stripos($bot, $_SERVER['HTTP_USER_AGENT']) !== false) {
 		define("CMS_IS_BOT", true);
 		break;
 	}
