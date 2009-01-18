0ab1d3b7e101cc7b71952f1e671ac26b3137b6d3
WanWizard <WanWizard@f05d0674-473a-0410-9a19-83e10c34e0e8>
2009-01-18 00:31:46 +0000

    added the island Réunion to the countrycode list

--- administration/tools/language_pack_Nederlands.php
+++ administration/tools/language_pack_Nederlands.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /*---------------------------------------------------------------------+
 | ExiteCMS Content Management System                                   |
 +----------------------------------------------------------------------+
@@ -1544,6 +1544,7 @@ if (!function_exists('install_language_pack')) {
 		$localestrings['pw'] = "Palau";
 		$localestrings['py'] = "Paraguay";
 		$localestrings['qa'] = "Qatar";
+		$localestrings['re'] = "Réunion";
 		$localestrings['ro'] = "Roemenië";
 		$localestrings['rs'] = "Servië";
 		$localestrings['ru'] = "Russische Federatie";
@@ -1799,6 +1800,7 @@ if (!function_exists('install_language_pack')) {
 		$localestrings['Qatar'] = "qa";
 		$localestrings['Republic Of Korea'] = "kr";
 		$localestrings['Republic Of Moldova'] = "md";
+		$localestrings['Réunion'] = "re";
 		$localestrings['Romania'] = "ro";
 		$localestrings['russia'] = "ru";
 		$localestrings['Russian Federation'] = "ru";
@@ -3776,7 +3778,7 @@ if (!defined('CMS_SETUP')) require_once dirname(__FILE__)."/../../includes/core_
 if (!function_exists('load_localestrings')) {
 	function load_localestrings($localestrings, $locales_code, $locales_name, $step="") {
 		global $db_prefix, $_db_log;
-	
+
 		// if this is an upgrade, remove the old locale strings first
 		if ($step == "upgrade") {
 			$result = dbquery("DELETE FROM ".$db_prefix."locales WHERE locales_code = '$locales_code' AND locales_name = '$locales_name'");
@@ -3819,7 +3821,7 @@ if (!defined('LP_SKIP_MAIN')) {
 		// load the locale for this module
 		locale_load("admin.main");
 	}
-	
+
 	if (defined('CMS_SETUP') && !defined('CMS_SETUP_LOAD')) {
 		// skip if in setup and no load request was given
 		$step = "skip";
@@ -3830,16 +3832,16 @@ if (!defined('LP_SKIP_MAIN')) {
 		//else load the theme functions for interactive mode
 		require_once PATH_INCLUDES."theme_functions.php";
 	}
-	
+
 	// used to store template variables
 	$variables = array();
-	
+
 	// message variable
 	$variables['message'] = "";
-	
+
 	// make sure step has a value
 	if (!isset($step)) $step = "";
-	
+
 	// de-install the language pack
 	if ($step == "remove") {
 		// check if the locale exists
@@ -3859,7 +3861,7 @@ if (!defined('LP_SKIP_MAIN')) {
 			}
 		}
 	}
-	
+
 	// install the language pack
 	if ($step == "install" || $step == "upgrade") {
 		if ($step == "install" || defined('CMS_SETUP_LOAD')) {
@@ -3883,36 +3885,36 @@ if (!defined('LP_SKIP_MAIN')) {
 			}
 		}
 	}
-	
+
 	if (defined('CMS_SETUP')) {
-	
+
 		// no output at all in setup mode
-		
+
 	} elseif (CMS_CLI) {
-	
+
 		// output in CLI mode
 		echo "Language pack installed.\n";
-		
+
 	} else {
-	
+
 		// interactive mode
 		require_once PATH_INCLUDES."theme_functions.php";
-	
+
 		// countries for which this language pack applies
 		$variables['flags'] = explode("|", LP_COUNTRIES);
 
 		// check the last update of the locale
 		$variables['last_update'] = isset($settings['LanguagePack.'.LP_LANGUAGE]) ? $settings['LanguagePack.'.LP_LANGUAGE] : min(LP_DATE - 1, dbfunction("MAX(locales_datestamp)", "locales", "locales_code = '".LP_LOCALE."' AND locales_name NOT LIKE 'modules%'"));
-		
+
 		// check if this language pack has been installed
 		$variables['can_install'] = dbcount("(*)", "locale", "locale_code = '".LP_LOCALE."'") == 0;
 		$variables['can_remove'] = LP_LOCALE != $settings['default_locale'] && $variables['can_install'] == false;
 		$variables['can_upgrade'] = $variables['can_install'] == false && $variables['last_update'] < LP_DATE;
-	
+
 		// define the body panel variables
 		$template_panels[] = array('type' => 'body', 'name' => 'admin.tools.languagepack', 'title' => "ExiteCMS Language Packs", 'template' => 'admin.tools.languagepack.tpl', 'locale' => "admin.main");
 		$template_variables['admin.tools.languagepack'] = $variables;
-		
+
 		// Call the theme code to generate the output for this webpage
 		require_once PATH_THEME."/theme.php";
 	}
