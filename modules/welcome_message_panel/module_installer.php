3acc75af4ece758f2c9d746112e77c27481ad303
root <root@f05d0674-473a-0410-9a19-83e10c34e0e8>
2010-03-17 19:11:49 +0000

    update of the ExiteCMS core module installers to version 7.3 (maintenance release)

--- modules/welcome_message_panel/module_installer.php
+++ modules/welcome_message_panel/module_installer.php
@@ -23,7 +23,7 @@ if (!checkrights("I") || !defined("iAUTH") || $aid != iAUTH || !defined('INIT_CM
 +----------------------------------------------------*/
 $mod_title = "Welcome message panel";
 $mod_description = "Body panel to display the site's welcome message";
-$mod_version = "1.1.0";
+$mod_version = "1.1.1";
 $mod_developer = "WanWizard";
 $mod_email = "wanwizard@exitecms.org";
 $mod_weburl = "http://www.exitecms.org/";
@@ -46,8 +46,8 @@ if (str_replace(".", "", $settings['version']) < 720) {
 	$mod_errors .= sprintf($locale['mod001'], '7.20');
 }
 // check for a maximum version of the ExiteCMS engine
-if (str_replace(".", "", $settings['version']) > 720) {
-	$mod_errors .= sprintf($locale['mod002'], '7.20');
+if (str_replace(".", "", $settings['version']) > 730) {
+	$mod_errors .= sprintf($locale['mod002'], '7.30');
 }
 // check for a specific revision number range that is supported
 if ($settings['revision'] < 0 || $settings['revision'] > 999999) {
